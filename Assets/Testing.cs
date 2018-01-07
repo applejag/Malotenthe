773e72533a78c18048830183970ee171c53b600c
kalle (jag) <kalle.jillheden@me.com>
2018-01-07 22:34:50 +0100

    RingRaycast testing script

---
+++ Assets/Testing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : RingObject
+{
+	public float length = 1;
+	public LayerMask layerMask = 1;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+
+		RingRaycastHit hit;
+		if (RingRaycast(transform.position, transform.forward, out hit, length, layerMask))
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(hit.point, 0.2f);
+		}
+		
+		Vector3 last = transform.position;
+		foreach (Vector3 next in hit.prevPoints)
+		{
+			Gizmos.DrawLine(last, next);
+			last = next;
+		}
+		Gizmos.DrawLine(last, hit.point);
+
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawRay(transform.position, transform.forward * length);
+	}
+}
